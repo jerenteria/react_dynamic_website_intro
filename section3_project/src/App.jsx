d20de185235694c6683985c1c11d880817afe6db
jerenteria <renteria.juan96@gmail.com>
2023-12-21 23:02:47 -0800

    added core concepts section and getting a better understanding of props

--- section3_project/src/App.jsx
+++ section3_project/src/App.jsx
@@ -1,4 +1,7 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+
+
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -24,13 +27,37 @@ function Header() {
   );
 }
 
+// renders the items in core concepts section by receiving props(html/jsx attributes) set below like title, description, image
+// you can use one function to render the same component but with different data by setting the data with key value pairs with different
+// data like in the core concepts section in jsx
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept 
+          title="Components"
+          description="The Core UI building block."
+          image={componentsImg}
+          />
+          <CoreConcept />
+          <CoreConcept />
+          <CoreConcept />
+        </ul>
+        </section>
       </main>
     </div>
   );
