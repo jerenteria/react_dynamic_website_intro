a4eb63f2603c84b2dd1198e3a6b3b8b79287987d
jerenteria <renteria.juan96@gmail.com>
2023-12-27 21:16:48 -0800

    working on tabs

--- section3_project/src/App.jsx
+++ section3_project/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header.jsx';
 import { CORE_CONCEPTS } from './data.js';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
   return (
@@ -30,6 +31,15 @@ function App() {
           image={CORE_CONCEPTS[3].image}
           />
         </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
---
+++ section3_project/src/components/TabButton.jsx
@@ -0,0 +1,10 @@
+export default function TabButton(props) {
+    function handleClick() {
+        
+    };
+    return (
+        <li>
+            <button onClick={handleClick}>{props.children}</button>
+        </li>
+    );
+}
